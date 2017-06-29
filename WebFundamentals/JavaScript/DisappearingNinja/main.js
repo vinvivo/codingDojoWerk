ab8bed763faaf3346e45c599942fac44cb9e76eb
vinney <vinvivo@users.noreply.github.com>
2017-06-28 22:06:54 -0700

    Updated meta tags for 'jQuery Function'

---
+++ WebFundamentals/JavaScript/DisappearingNinja/main.js
@@ -0,0 +1,9 @@
+/*jslint browser: true*/
+/*jslint node: true */
+/*global $, jQuery, alert*/
+//JavaScript document for index-jQueryFunctions.html
+"use strict";
+
+$(document).ready(function () {
+    
+});
