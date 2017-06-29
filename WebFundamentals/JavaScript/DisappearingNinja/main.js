9b476bd2e855f428dd9436ee8ed7efa871f6ff68
vinney <vinvivo@users.noreply.github.com>
2017-06-29 04:12:56 -0700

    Added 'Disappearing Ninja' assignment to WebFundamentals/JavaScript

--- WebFundamentals/JavaScript/DisappearingNinja/main.js
+++ WebFundamentals/JavaScript/DisappearingNinja/main.js
@@ -5,5 +5,12 @@
 "use strict";
 
 $(document).ready(function () {
-    
+    //hide images on click of image
+    $(".ninjaCat").click(function () {
+        $(this).hide();
+    });
+    //retore all images on click of button
+    $("button").click(function () {
+        $(".ninjaCat").show();
+    });
 });
