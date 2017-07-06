2375caf4405c0f6e10b59fef268f3a9f7e1409b3
vinney <vinvivo@users.noreply.github.com>
2017-07-05 20:36:26 -0700

    Refactored 'Car' assignment in /Python

--- Python/car.py
+++ Python/car.py
@@ -14,17 +14,19 @@
 
 
 class Car(object):
-    def __init__(self, price, speed, fuel, mileage, tax = 0.12):
+    def __init__(self, price, speed, fuel, mileage):
         self.price = price  # in $USD
         self.speed = speed  # in miles per gallon
         self.fuel = fuel    # qualitative values
         self.mileage = mileage
-        self.tax = tax
         if self.price > 10000:
             self.tax = 0.15
+        else:
+            self.tax = 0.12
+        self.display_all()
 
     def display_all(self):
-        print "Price: ${}; Speed: {} mph; Fuel: {}; Mileage: {} miles; Tax: {}%".format(self.price, self.speed, self.fuel, self.miles, self.tax * 100)
+        print "Price: ${}; Speed: {} mph; Fuel: {}; Mileage: {} miles; Tax: {}%".format(self.price, self.speed, self.fuel, self.mileage, self.tax * 100)
 
 
 toyota = Car(10000, 90, "Full", 40)
@@ -34,9 +36,3 @@ peugeot = Car(8800, 80, "Full", 30)
 fisker = Car(109800, 140, "Empty", 30)
 renault = Car(9000, 80, "Half full", 33)
 
-toyota.display_all()
-tesla.display_all()
-alfa_romeo.display_all()
-peugeot.display_all()
-fisker.display_all()
-renault.display_all()
--- Python/test.py
+++ Python/test.py
@@ -1,12 +1,14 @@
 class Car(object):
-    def __init__(self, price, speed, fuel, mileage, tax = 0.12):
+    def __init__(self, price, speed, fuel, mileage):
         self.price = price  # in $USD
         self.speed = speed  # in miles per gallon
         self.fuel = fuel    # qualitative values
         self.mileage = mileage
-        self.tax = tax
         if self.price > 10000:
             self.tax = 0.15
+        else:
+            self.tax = 0.12
+        self.display_all()
 
     def display_all(self):
         print "Price: ${}; Speed: {} mph; Fuel: {}; Mileage: {} miles; Tax: {}%".format(self.price, self.speed, self.fuel, self.mileage, self.tax * 100)
@@ -18,10 +20,3 @@ alfa_romeo = Car(41999, 95, "Half full", 25)
 peugeot = Car(8800, 80, "Full", 30)
 fisker = Car(109800, 140, "Empty", 30)
 renault = Car(9000, 80, "Half full", 33)
-
-toyota.display_all()
-tesla.display_all()
-alfa_romeo.display_all()
-peugeot.display_all()
-fisker.display_all()
-renault.display_all()
