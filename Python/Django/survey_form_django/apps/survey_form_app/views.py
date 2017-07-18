b414d861af63100d4c571cb56733e26daa8d5742
vinney <vinvivo@users.noreply.github.com>
2017-07-17 19:08:57 -0700

    Completed 'Survey Form' for Python/Django

---
+++ Python/Django/survey_form_django/apps/survey_form_app/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render, HttpResponse, redirect
+
+def index(request):
+    return render(request, "index.html")
+
+def process(request):
+    request.session['name'] = request.POST['name']
+    request.session['location'] = request.POST['location']
+    request.session['language'] = request.POST['language']
+    request.session['comment'] = request.POST['comment']
+    return redirect("/result")
+
+def result(request):
+    if not 'counter' in request.session:
+        request.session['counter'] = 1
+    request.session['counter'] += 1
+    return render(request, "result.html")
