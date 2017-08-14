b1dff5fd6c82ba7cfa094a413c305695ec359f36
vinney <vinvivo@users.noreply.github.com>
2017-08-14 13:58:58 -0700

    Completed 'Time Zone Display' for MEAN-Angular

--- MEAN/Angular/timeZoneDisplay/src/app/app.component.ts
+++ MEAN/Angular/timeZoneDisplay/src/app/app.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'US Time Zone Display';
+  currentTime = Date.now();
+  time = null;
+  clicked = null;
+
+  clickZone(timeZone) {
+    console.log("clickZone("+timeZone+")");
+    this.time = new Date();
+    console.log(this.time.getHours());
+    if (timeZone === "Pacific") {
+      this.time = this.time.setHours(this.time.getHours());
+    } else if (timeZone === "Mountain") {
+      this.time = this.time.setHours(this.time.getHours()+1);
+    } else if (timeZone === "Central") {
+      this.time = this.time.setHours(this.time.getHours()+2);
+    } else if (timeZone === "Eastern") {
+      this.time = this.time.setHours(this.time.getHours()+3);
+    } else if (timeZone === "clear") {
+      this.time = null;
+    }
+    this.clicked = timeZone;
+  };
+
+  
 }
