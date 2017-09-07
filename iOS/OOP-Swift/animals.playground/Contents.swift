dc1ae10549a71818d1e85f47f1a53964f73a38a9
vinney <vinvivo@users.noreply.github.com>
2017-09-06 18:41:00 -0700

    Completed "Animals" for OOP-Swift #iOS

--- iOS/OOP-Swift/animals.playground/Contents.swift
+++ iOS/OOP-Swift/animals.playground/Contents.swift
@@ -13,7 +13,7 @@ class Animal {
         self.name = name
     }
     func displayHealth() {
-        print("Health = \(self.health).")
+        print("\(self.name)'s health = \(self.health).")
     }
 }
 var myAnimal = Animal(name: "Dot")
@@ -21,27 +21,70 @@ myAnimal.displayHealth()
 
 // Next, create a subclass of Animal called Cat
 class Cat: Animal {
-                        // Modify the cat's health to be 150
-    override var health: Int {
-                        // Get the value from the super class
-        get {
-            return 150  // modify the value
-        }
-        set {}          // leave this blank
+    init(catName: String) {
+        super.init(name: catName)
+        self.health = 150           // Modify the cat's health to be 150
     }
-    
     func growl() -> String {
         return "Rawr!"
     }
     
     func run() {
-        print("Running ...")
-        return self.health -= 10
+        print("\(self.name) is running ...")
+        self.health -= 10
     }
 }
 
-var myCat = Cat(name: "Lola")
+// Create two subclasses of Cat - Cheetah and Lion
+class Cheetah: Cat {
+    func sleep() {
+        for _ in 1...50 {
+            self.health += 1
+            if self.health == 200 {
+                break
+            }
+        }
+    }
+    
+    override func run() {
+        if self.health >= 50 {
+            print("\(self.name) is running fast, watch out !!!")
+            self.health -= 50
+        } else {
+            print("\(self.name) doesn't have enough energy to run.")
+        }
+    }
+}
+
+class Lion: Cat {
+    init(lionName: String) {
+        super.init(catName: lionName)
+        self.health = 200
+    }
+    override func growl() -> String {
+        print("ROAR!!!  I am the King of the Jungle, henny!")
+        return "ROAR!!!  I am the King of the Jungle, henny!"
+    }
+}
+
+
+var myCat = Cat(catName: "Lola")
 myCat.displayHealth()
 myCat.growl()
 myCat.run()
 myCat.displayHealth()
+
+var chester = Cheetah(catName: "Chester")
+chester.displayHealth()
+chester.run()
+chester.run()
+chester.run()
+chester.run()
+chester.displayHealth()
+
+var leesa = Lion(lionName: "Leesa")
+leesa.run()
+leesa.run()
+leesa.run()
+leesa.growl()
+
