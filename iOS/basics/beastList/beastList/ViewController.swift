a3f26a15540ff823b13df6cedafdb5c8c0b0f5b2
vinney <vinvivo@users.noreply.github.com>
2017-09-11 16:09:24 -0700

    Able to create new table rows from text field input.

--- iOS/basics/beastList/beastList/ViewController.swift
+++ iOS/basics/beastList/beastList/ViewController.swift
@@ -8,16 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
 
+    var tasks = ["Something cool", "Something very cool", "Something extremely cool"]
+    
+    @IBOutlet weak var taskTextField: UITextField!
+    @IBOutlet weak var tableView: UITableView!
+    @IBAction func beastButtonPressed(_ sender: UIButton) {
+        if let textInput = taskTextField.text {
+        tasks.append(textInput)
+        }
+        tableView.reloadData()
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+//--Set our UITableView dataSource to self
+        tableView.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
+        cell.textLabel?.text = "\(tasks[indexPath.row])"
+        return cell
     }
 
 
